a400ae980580691b2d61df1e3f3f0d301cede583
SamVerwoerd <302324869@student.rocmondriaan.nl>
2021-03-15 04:45:39 +0100

    Trolls

---
+++ Card.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectHigherLower
+{
+    // Vervang de class Card met de Card van card-assignment opdracht zodra die af is.
+    class Card
+    {
+        /*
+         * Attributten bepalen
+         *  
+         * Hoe weet je nu welke attributes een class moet hebben?
+         *
+         * De attributes van een class beschrijven waar het object uit moet bestaan.
+         *
+         * Een kaart heeft bijvoorbeeld een symbol, harten, schoppen, ruiten of klaveren.
+         * De engelse benaming hiervoor is een suit.
+         * Een suit bestaat vaak uit de kleur rood of zwart. Deze variabele noemen we colour.
+         * Verder bestaat het uit een getal of letter, 2 - 10, J, Q, K, A. Deze variabele noemen we value.
+         * Dit getal of deze letter heeft vaak ook een waarde, vaak kan je de J zien als 11, Q als 12, K als 13, A als 14. Echter verschilt dit nog wel eens per kaarspel.
+         * Er moet een punten op de kaart zitten om de waarde te bepalen of een kaart hoger is dan een andere. Deze variabele noemen we points.
+         */
+        // Attributes
+        private string suit;
+        private string colour;
+        private string value;
+        private int points;
+        private static string[] allowedValues = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+        /*
+         * Constructor
+         */
+        public Card(string value, string suit, int points)
+        {
+            this.SetValue(value);
+            this.SetSuit(suit);
+            this.points = points;
+        }
+
+        // TODO #2 This method should be implemented.
+        /**
+         * This method prints the card e.g. 2♡ or J♤
+         */
+        public void PrintCard()
+        {
+            Console.Write(" 2♡ ");
+
+        }
+
+        // TODO #1 This method seems to be broken. Can someone fix this please?
+        /**
+         * Checks wether the value is in the allowedValues list.
+         * If it is this method returns true. - gedaan
+         */
+        public bool CheckValue(string value)
+        {
+            bool isAllowed = false;
+            for (int i = 0; i < allowedValues.Length; i++)
+            {
+                if (value == allowedValues[i])
+                {
+                    isAllowed = true;
+                }
+            }
+            return isAllowed;
+        }
+
+        /**
+         * This method returns the points of this card. 
+         */
+        public int GetPoints()
+        {
+            return points;
+        }
+
+        /**
+         * This method sets the Suit of the card AND the colour that represents that suit.
+         */
+        public void SetSuit(string suit)
+        {
+            if (suit == "♡" || suit == "♢" || suit == "♧" || suit == "♤")
+            {
+                this.suit = suit;
+                this.colour = GetColour(suit);
+            }
+            else
+            {
+                throw new Exception("Could not find the specified suit.");
+            }
+        }
+
+        /**
+         * This method checks the value and if the value is allowed it sets the value.
+         * This method method uses the CheckValue method to check if the value is correct.
+         */
+        public void SetValue(string value)
+        {
+            if (CheckValue(value))
+            {
+                this.value = value;
+            }
+            else
+            {
+                throw new Exception("This value of a card is not allowed");
+            }
+        }
+
+        /**
+         * This method returns the color. 
+         * You can use this method with and suit argument to translate a suit to colour 
+         */
+        public string GetColour()
+        {
+            return this.colour;
+        }
+
+        /**
+         * This method translate a suit to the corresponsing color.
+         * Use this method to get the colour of a suit
+         */
+        public string GetColour(string suit)
+        {
+            if (suit == "♡" || suit == "♢")
+            {
+                return "red";
+            }
+            else if (suit == "♧" || suit == "♤")
+            {
+                return "black";
+            }
+            else
+            {
+                throw new Exception("Could not find the specified suit.");
+            }
+        }
+
+        /**
+         * Returns all the allowed Card Values
+         */
+        public static string[] GetAllowedCardValues()
+        {
+            return allowedValues;
+        }
+
+        public static void Cardmake() 
+        { 
+          
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace ProjectHigherLower
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Variables
+            
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Age Check                    
+            Program.WriteLineSlow("Welcome to H & L - The card guessing game.");
+
+            // Info 
+            Console.Clear();
+            Console.WriteLine("Please enter a name");
+            string Name = Console.ReadLine();
+            Program.WriteLineSlow("Your name has been saved as " + Name + ".");
+
+            Console.ResetColor();
+            // Rules & Instructions
+            Console.Clear();
+            Program.WriteLineSlow("How to play Higher lower");
+            Program.WriteLineSlow("Its simple");
+            Program.WriteLineSlow("You need to guess if the card is higher or lower.");
+            Program.WriteLineSlow("You get 1 point for each right answer.");
+
+            // start
+            Program.WriteLineSlow("The computer is generating the deck of cards...");
+            int score = 0; 
+            bool temp = true;
+            string Continue;
+            while (temp == true)
+            {
+               
+
+                string[] card2 = {  "\u2666", "\u2665", "\u2663", "\u2660" };
+                string[] cardValue = Card.GetAllowedCardValues();
+       
+
+                Random rnd = new Random();
+                string[] card3 = { cardValue[rnd.Next(0, 13)], card2[rnd.Next(0, 4)] };
+                string[] card4 = { cardValue[rnd.Next(0, 13)], card2[rnd.Next(0, 4)] };
+
+                int cardmake3()
+                {
+                    int cijfer = 3;
+                    int plus = 0;
+                    for (int i = 2; i < 15; i++)
+                    {
+                        if (card3[0] == cardValue[plus])
+                        {
+                            cijfer = i;
+                            plus++;
+                        }
+                        else
+                        {
+                            plus++;
+                        }
+
+                    }
+                    return cijfer;
+                }
+
+                int cardmake2()
+                {
+                    int cijfer = 3;
+                    int plus = 0;
+                    for (int i = 2; i < 15; i++)
+                    {
+                        if (card4[0] == cardValue[plus])
+                        {
+                            cijfer = i;
+                            plus++;
+                        }
+                        else
+                        {
+                            plus++;
+                        }
+
+                    }
+                    return cijfer;
+                }
+                Console.Clear();
+                Console.WriteLine("card: ");
+                if (cardmake3() >= 10)
+                {
+                  Console.WriteLine(cardmake3());
+                }
+                else
+                {
+                  Console.WriteLine(cardmake3());
+                }
+                Console.WriteLine(card3[1]);
+                Console.WriteLine(Name + " choose, h or l (Higher or Lower)");
+                string guess = Console.ReadLine();
+                if (guess == "H") 
+                {
+                    guess = "h";
+                }
+                else if (guess == "L")
+                {
+                    guess = "l";
+                }
+                if (guess == "h" && cardmake3() < cardmake2())
+                {
+                    //goed antwoord
+                    Console.WriteLine("the card was : ");
+                    if (cardmake3() >= 10)
+                    {
+                        Console.WriteLine(cardmake2());
+                    }
+                    else
+                    {
+                        Console.WriteLine(cardmake2());
+                    }
+                    Console.WriteLine(card4[1]);
+                    score++;
+                    Program.WriteLineSlow("good");
+                    Program.WriteLineSlow("your score is now: " + score);
+
+
+                    Console.Clear();
+
+                }
+                else if (guess == "h" && cardmake3() > cardmake2())
+                {
+                    //fout antwoord
+                    Console.WriteLine("the card was : ");
+                    if (cardmake3() >= 10)
+                    {
+                        Console.WriteLine(cardmake2());
+                    }
+                    else
+                    {
+                        Console.WriteLine(cardmake2());
+                    }
+                    Console.WriteLine(card4[1]);
+                    Program.WriteLineSlow("wrong");
+   
+                    Console.Clear();
+                    Program.WriteLineSlow("Do you want to continue? ");
+                    Program.WriteLineSlow("Type n if you would like to stop");
+                    Program.WriteLineSlow("Type y if you would like to continue");
+                    Continue = Console.ReadLine();
+                    if ((Continue == "n") || (Continue == "N"))
+                    {
+                        Program.WriteLineSlow("Your final score is: " + score);
+                        temp = false;
+                        
+                    }
+                }
+                else if (guess == "l" && cardmake3() > cardmake2())
+                {
+                    //goed antwoord
+                    Console.WriteLine("the card was : ");
+                    if (cardmake3() >= 10)
+                    {
+                        Console.WriteLine(cardmake2());
+                    }
+                    else
+                    {
+                        Console.WriteLine(cardmake2());
+                    }
+                    Console.WriteLine(card4[1]);
+                    Program.WriteLineSlow("good");
+                    score++;
+                    Program.WriteLineSlow("your score is now: " + score);
+
+                    Console.Clear();
+                }
+
+                else if (guess == "l" && cardmake3() < cardmake2())
+                {
+                    //fout antwoord
+                    Console.WriteLine("the card was : ");
+                    if (cardmake3() >= 10)
+                    {
+                        Console.WriteLine(cardmake2());
+                    }
+                    else
+                    {
+                        Console.WriteLine(cardmake2());
+                    }
+                    Console.WriteLine(card4[1]);
+                    Program.WriteLineSlow("wrong");
+  
+                    Console.Clear();
+                    Program.WriteLineSlow("Do you want to continue? ");
+                    Program.WriteLineSlow("Type n if you would like to stop");
+                    Program.WriteLineSlow("Type y if you would like to continue");
+                    Continue = Console.ReadLine();
+                    if ((Continue == "n") ||(Continue == "N"))
+                    {
+                        temp = false;
+                    }
+                }
+                else if (cardmake3() == cardmake2())
+                {
+                    // kaart is gelijk
+                    Console.WriteLine("the card was : ");
+                    if (cardmake3() >= 10)
+                    {
+                        Console.WriteLine(cardmake2());
+                    }
+                    else
+                    {
+                        Console.WriteLine(cardmake2());
+                    }
+                    Console.WriteLine(card4[1]);
+                    Program.WriteLineSlow("equal");
+                    score++;
+                    Program.WriteLineSlow("your score is now: " + score);
+
+                    Console.Clear();
+                }
+            }
+        }
+            public static void WriteLineSlow(string message)
+            {
+                Console.WriteLine(message);
+                Thread.Sleep(1500);
+            }
+    }
+}
